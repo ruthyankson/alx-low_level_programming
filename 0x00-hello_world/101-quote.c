87ac1daa847e444548c40a5643f3a82b6d6dbd13
AdwowaRay <ruthyankson357@gmail.com>
2023-02-16 18:34:34 -0800

    100 and 101 updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  *main - Entry point
  *Return: Once 1 (Error)
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
 return (1);
 }
