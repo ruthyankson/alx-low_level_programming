823a0f87a835a8c837de0cf53a4c89ad1e7d2754
AdwowaRay <ruthyankson357@gmail.com>
2023-02-21 18:52:47 -0800

    5-sign.c updated removed spaces and commas

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,16 @@ int print_sign(int n)
 	if (n > '0')
 	{
 		_putchar(43);
-		_putchar(44);
-		_putchar(32);
 		return (1);
 	}
 	else if (n == '0')
 	{
 		_putchar(48);
-		_putchar(44);
-		_putchar(32);
 		return (0);
 	}
 	else
 	{
 		_putchar(45);
-		_putchar(44);
-		_putchar(32);
 		return (-1);
 	}
 }
