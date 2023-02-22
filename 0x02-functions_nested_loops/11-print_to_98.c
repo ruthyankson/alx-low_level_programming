002e091d56c2dd0c929809deefb7d38add259d91
AdwowaRay <ruthyankson357@gmail.com>
2023-02-22 07:18:54 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,45 +7,32 @@
 
 void print_to_98(int n)
 {
-	int max_lim = 98, num;
+	int max_lim = 98, num, l_num;
 
-	if (n < max_lim)
+	while (n != max_lim)
 	{
-		while (n <= max_lim)
+		num = n;
+		if (num < 0)
 		{
-			num = n;
-			while ((num / 10) != 0)
-			{
-				_putchar((num % 10) + '0');
-				num = ((num - (num % 10)) / 10);
-			}
-			_putchar(num + '0');
-			if (n != max_lim)
-			{
-				_putchar(',');
-				_putchar(32);
-			}
+			_putchar('-');
+			num = -num;
+		}
+		while ((num / 10) != 0)
+		{
+			l_num = num % 10;
+			num = ((num - l_num) / 10);
+		}
+		_putchar(num + '0');
+		_putchar(l_num + '0');
+		if (n != max_lim)
+		{
+			_putchar(',');
+			_putchar(32);
+		}
+		if (n < max_lim)
 			n++;
-		}
-	}
-	else if (n > max_lim)
-	{
-		while (n >= max_lim)
-		{
-			num = (n / 10);
-			_putchar((num % 10) + '0');
-			if (n != max_lim)
-			{
-				_putchar(',');
-				_putchar(32);
-			}
+		else if (n > max_lim)
 			n--;
-		}
-	}
-	else
-	{
-		_putchar(57);
-		_putchar(56);
 	}
 	_putchar('\n');
 }
