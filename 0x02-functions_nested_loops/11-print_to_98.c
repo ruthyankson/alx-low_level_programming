3492d4425b022a885b4dc21e52d8c78202cae7c8
AdwowaRay <ruthyankson357@gmail.com>
2023-02-21 22:10:52 -0800

    11-print_to_98.c added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *@n: minimum limit input
+ */
+
+void print_to_98(int n)
+{
+	int max_lim = 98, num;
+
+	if (n < max_lim)
+	{
+		while (n <= max_lim)
+		{
+			num = (n / 10);
+			_putchar((num % 10) + '0');
+			if (n != max_lim)
+			{
+				_putchar(',');
+				_putchar(32);
+			}
+			n++;
+		}
+	}
+	else if (n > max_lim)
+	{
+		while (n >= max_lim)
+		{
+			num = (n / 10);
+			_putchar(num + '0');
+			_putchar((n % 10) + '0');
+			if (n != max_lim)
+			{
+				_putchar(',');
+				_putchar(32);
+			}
+			n--;
+		}
+	}
+	else
+	{
+		_putchar(57);
+		_putchar(56);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -38,3 +38,6 @@ Prints the 9 times table, starting with 0.
 
 ## 10-add.c
 Adds two integers and returns the result.
+
+## 11-print_to_98.c
+Prints all natural numbers from n to 98
