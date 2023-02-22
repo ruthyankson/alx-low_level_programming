d2eeaa99f8274a25e0c832488f918d65e3709ea6
AdwowaRay <ruthyankson357@gmail.com>
2023-02-21 16:12:33 -0800

    2-print_alphabet_x10.c added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- *void print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
+
 void print_alphabet_x10(void)
 {
-	int ch = 97;
+	int ch = 'a';
 	int times = 10;
 
 	while (times > 0)
 	{
-		while (ch <= 122)
+		while (ch <= 'z')
 		{
-			putchar(ch);
+			_putchar(ch);
 			ch++;
 		}
 		times--;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
