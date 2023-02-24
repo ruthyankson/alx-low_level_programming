c02da6b598781cac5e058781c410d2adcd12fffc
AdwowaRay <ruthyankson357@gmail.com>
2023-02-24 08:55:54 -0800

    stdio and putchar changed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main - fizz_buzz program
  *Return: 0, Always success
@@ -11,42 +11,42 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			putchar('F');
-			putchar('i');
-			putchar('z');
-			putchar('z');
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
 		}
 		else if (i % 5 == 0)
 		{
-			putchar('B');
-			putchar('u');
-			putchar('z');
-			putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
 		}
 		else if (i % 15 == 0)
 		{
-			putchar('F');
-			putchar('i');
-			putchar('z');
-			putchar('z');
-			putchar('B');
-			putchar('u');
-			putchar('z');
-			putchar('z');
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
 		}
 		else
 		{
 			if (i > 9 && i < 100)
-				putchar((i / 10) + '0');
+				_putchar((i / 10) + '0');
 			else if (i == 100)
 			{
-				putchar((i / 100) + '0');
-				putchar((i % 100) + '0');
+				_putchar((i / 100) + '0');
+				_putchar((i % 100) + '0');
 			}
-			putchar((i % 10) + '0');
+			_putchar((i % 10) + '0');
 		}
-		putchar(32);
+		_putchar(32);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
