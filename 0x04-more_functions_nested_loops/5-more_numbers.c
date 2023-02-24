ddcaf5537eb513c700bda8344120ac21c4791413
AdwowaRay <ruthyankson357@gmail.com>
2023-02-23 20:48:31 -0800

    5-more_numbers.c added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,5 +4,19 @@
  */
 void more_numbers(void)
 {
-	
+	int count = 0;
+	int i = 0;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
