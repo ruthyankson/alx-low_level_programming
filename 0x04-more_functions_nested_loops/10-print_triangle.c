1959038d5fceee56180c378646e7da314523ceae
AdwowaRay <ruthyankson357@gmail.com>
2023-02-24 16:56:14 -0800

    for loop added for left spaces

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,12 +5,16 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, space;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
+			for (space = 0; space < size - (i + 1); space++)
+			{
+				_putchar(32);
+			}
 			for (j = 0; j < i + 1; j++)
 			{
 				_putchar('#');
