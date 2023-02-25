2af5904438936eee6ac08e6d71faa81e0c1bff5e
AdwowaRay <ruthyankson357@gmail.com>
2023-02-24 19:32:10 -0800

    leap year check in place

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 400 == 0)
+	int mod4 = year % 4;
+	int mod400 = year % 400;
+	int mod100 = year % 100;
+
+	if ((mod4 == 0 && (!(mod100 == 0))) || (mod4 == 0 && mod400 == 0 && mod100 == 0))
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
