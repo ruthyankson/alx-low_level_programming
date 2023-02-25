afb33ab4b3da2e35bbf3d4b7498a986cf79b0e01
AdwowaRay <ruthyankson357@gmail.com>
2023-02-24 20:36:52 -0800

    3-print_remaining_days.c updated

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,6 +19,10 @@ void print_remaining_days(int month, int day, int year)
 	if ((mod4 == 0 && (!(mod100 == 0))) ||
 	    (mod4 == 0 && mod400 == 0 && mod100 == 0))
 	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
