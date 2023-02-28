7740cc8d9ead173b980e28acf8f775537ba3a834
AdwowaRay <ruthyankson357@gmail.com>
2023-02-27 19:57:44 -0800

    2-strlen.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ *@
+ *Return: string length as integer
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -9,3 +9,6 @@ Takes a pointer to an int as parameter and updates the value it points to to 98.
 
 ## 1-swap.c
 Swaps the values of two integers.
+
+## 2-strlen.c
+Returns the length of a string.
