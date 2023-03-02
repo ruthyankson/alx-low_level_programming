f8023a93278087e61356ef368f245aa05b240704
AdwowaRay <ruthyankson357@gmail.com>
2023-03-02 05:13:41 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,5 +5,16 @@
  */
 void print_number(int n)
 {
-	
+	unsigned int un = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		un = -1 * n;
+	}
+	if (un / 10 != 0)
+	{
+		print_number(un / 10);
+	}
+	_putchar((un % 10) + '0');
 }
