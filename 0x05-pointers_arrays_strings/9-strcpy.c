82dec3b66d7c677e2d10ccd14ddbe663ddb0ed2f
AdwowaRay <ruthyankson357@gmail.com>
2023-03-01 16:11:53 -0800

    9-strcpy.c updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,10 @@
  *_strcpy - coping src, into the buffer pointed to by dest.
  *@dest: Destination
  *@src: Source
- *Return:
+ *Return: charater pointer
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
 	char *ret_ptr = dest;
 
 	while (*src != '\0')
