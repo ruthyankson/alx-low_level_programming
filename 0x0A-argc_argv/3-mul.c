a520a9744f0f2c9a9b218d63d1d09a0f6812b293
AdwowaRay <ruthyankson357@gmail.com>
2023-03-10 17:30:35 -0800

    indentation propoerly don in 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
-        int result, num1, num2;
-        if (argc < 3 || argc > 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        num1 = _atoi(argv[1]);
-        num2 = _atoi(argv[2]);
-        result = num1 * num2;
-        printf("%d\n", result);
-        return (0);
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
 
 /**
@@ -29,6 +30,7 @@ int main(int argc, char *argv[])
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
