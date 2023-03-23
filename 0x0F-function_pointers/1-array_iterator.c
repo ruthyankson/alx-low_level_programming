9409f90125bf4c3a572045f96d86e055f200bcb1
AdwowaRay <ruthyankson357@gmail.com>
2023-03-22 18:52:17 -0700

    stddef.h added (1-array_iterator.c)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
+#include "function_pointers.h"
 /**
  * array_iterator - prints each array element on a newline
  * @array: array
