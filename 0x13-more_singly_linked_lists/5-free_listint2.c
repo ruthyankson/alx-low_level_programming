e5f3eb0ad2f3bfc243c3b8a2e29b7f885b85a11c
AdwowaRay <ruthyankson357@gmail.com>
2023-04-04 15:03:09 -0700

    5-free_listint2.c added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees a linked list
+ *@head: pointer to the linked list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		*head = temp_node;
+	}
+	*head = NULL;
+}
