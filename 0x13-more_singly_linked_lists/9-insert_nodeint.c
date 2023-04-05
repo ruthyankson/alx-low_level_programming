84d50adeb8610c707db757d0a7cfedbfebcf88d8
AdwowaRay <ruthyankson357@gmail.com>
2023-04-04 19:43:35 -0700

    9-insert_nodeint.c changed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,41 @@
-#include "lists.h"
+[200~#include "lists.h"
 
 /**
- *insert_nodeint_at_index - inserts a new node in a linked list
- *at a given position
- *@head: pointer to the first node in the list
- *@idx: the new node index
- *@n: data to insert in the new node
- *Returns: the address of the new node, or NULL if it failed
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp_node = *head;
-	listint_t *new_node = malloc(sizeof(listint_t));
-	unsigned int counter;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	if (!new_node || !head)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	for (counter = 0; temp_node && counter < idx; counter++)
+	for (i = 0; temp && i < idx; i++)
 	{
-		if (counter == (idx - 1))
+		if (i == idx - 1)
 		{
-			new_node->next = temp_node->next;
-			temp_node->next = new_node;
-			return (new_node);
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
 		else
-		{
-			temp_node = temp_node->next;
-		}
+			temp = temp->next;
 	}
 	return (NULL);
 }
---
+++ 0x13-more_singly_linked_lists/temp.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - inserts a new node in a linked list
+ *at a given position
+ *@head: pointer to the first node in the list
+ *@idx: the new node index
+ *@n: data to insert in the new node
+ *Returns: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp_node = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	unsigned int counter;
+
+	if (!new_node || !head)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (counter = 0; temp_node && counter < idx; counter++)
+	{
+		if (counter == (idx - 1))
+		{
+			new_node->next = temp_node->next;
+			temp_node->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			temp_node = temp_node->next;
+		}
+	}
+	return (NULL);
+}
