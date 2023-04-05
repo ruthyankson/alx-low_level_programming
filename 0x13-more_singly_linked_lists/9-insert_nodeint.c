2df15194da553aa2a918ee60a316f95ecf026f18
AdwowaRay <ruthyankson357@gmail.com>
2023-04-04 18:54:27 -0700

    9-insert_nodeint.c added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	unsigned int counter;
 
-	if (!new_node || !*head  || !head)
+	if (!new_node || !head)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
@@ -33,7 +33,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp_node->next = new_node;
 			return (new_node);
 		}
-		temp_node = temp_node->next;
+		else
+			temp_node = temp_node->next;
 	}
 	return (NULL);
 }
